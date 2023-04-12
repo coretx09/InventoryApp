568984fd34026f45a147b885023d3328f536a7a9
coretx09 <ngampiosauvet@gmail.com>
2023-04-13 02:48:04 +0300

    RV done

--- app/src/main/java/com/example/inventory/ItemListAdapter.kt
+++ app/src/main/java/com/example/inventory/ItemListAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.inventory
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
 import com.example.inventory.databinding.ItemListItemBinding
 
 class ItemListAdapter(private val onItemClicked:(Item) -> Unit):
@@ -12,24 +14,34 @@ class ItemListAdapter(private val onItemClicked:(Item) -> Unit):
 
 
     class ItemViewHolder(private var binding: ItemListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            binding.apply {
+                itemName.text = item.itemName
+                itemPrice.text = item.getFormattedPrice()
+                itemQuantity.text = item.quantityInStock.toString()
+            }
+
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        return ItemViewHolder(ItemListItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val current = getItem(position)
+        holder.itemView.setOnClickListener { onItemClicked(current) }
+        holder.bind(current)
     }
 
     companion object DiffCallback: DiffUtil.ItemCallback<Item>() {
         override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
-            TODO("Not yet implemented")
+            return oldItem === newItem
         }
 
         override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.itemName == newItem.itemName
         }
 
     }
